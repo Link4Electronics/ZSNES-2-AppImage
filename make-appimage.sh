a1722ecd2c6fee22b913c613ec2278341cff4c87
Link4Electronics <link4electronics@gmail.com>
2026-03-02 23:10:56 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,8 +8,9 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
+export ICON=https://raw.githubusercontent.com/xyproto/zsnes/refs/heads/main/icons/128x128x32.png
 export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export DEPLOY_OPENGL=1
 
 # Deploy dependencies
 quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
